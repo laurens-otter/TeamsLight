0f394538c317d459a14f621c7623aebb3cbf2d0e
laurens-otter <laurens_otter@live.nl>
2024-03-18 08:05:53 +0100

    Update Teamslight.ino

--- Teamslight.ino
+++ Teamslight.ino
@@ -1,44 +1,63 @@
 #include <FastLED.h>
 
 #define LED_PIN     0
-#define NUM_LEDS    7
+#define LED_COUNT   7
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
 
-CRGB leds[NUM_LEDS];
+CRGB leds[LED_COUNT];
+
+bool rainbowRunning = false;
 
 void setup() {
-  // Initialize serial communication
   Serial.begin(9600);
-
-  // Initialize FastLED library
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.clear();
-  FastLED.show();
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, LED_COUNT);
+  FastLED.show(); // Initialize all LEDs to 'off'
+  String lastcommand = "";
 }
 
 void loop() {
+String lastcommand = "";
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
-    if (command.equals("red")) {
-      setColor(CRGB::Red);
-    } else if (command.equals("yellow")) {
-      setColor(CRGB::Orange);
-    } else if (command.equals("green")) {
-      setColor(CRGB::Green);
-    } else if (command.equals("off")) {
-      FastLED.clear();
-      FastLED.show();
-    } else if (command.equals("off")) {
-      FastLED.clear();
-      FastLED.show();
+    command.trim();
+    String lastcommand = command; 
+    if (command.equalsIgnoreCase("red")) {
+      setColor(CRGB::Red); // Red
+    } else if (command.equalsIgnoreCase("yellow")) {
+      setrgb(255,43,0); // orange
+    } else if (command.equalsIgnoreCase("green")) {
+      setColor(CRGB::Green); // Green
+    } else if (command.equalsIgnoreCase("off")) {
+      setColor(CRGB::Black); // Turn off
+    } else if (command.equalsIgnoreCase("rgb")) {
     }
   }
+while(lastcommand == "rgb") {
+  for (int j = 0; j < 256; j++) {
+    for (int i = 0; i < LED_COUNT; i++) {
+      leds[i] = CHSV((j + (i * 10)) % 256, 255, 255);
+    }
+    FastLED.show();
+    delay(50);
+  }
+}    
+Serial.print(lastcommand);   
 }
 
 
-
 void setColor(CRGB color) {
-  for (int i = 0; i < NUM_LEDS; i++) {
+  for (int i = 0; i < LED_COUNT; i++) {
     leds[i] = color;
   }
   FastLED.show();
 }
+void setrgb(int red,int green,int blue) {
+  for (int i = 0; i < LED_COUNT; i++) {
+    leds[i].setRGB(red,green,blue);
+  }
+  FastLED.show();
+}
+
+
+
